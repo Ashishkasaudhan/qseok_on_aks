0be0a1708ce72221a2bcac108fd05c18dcc9597e
Christof Schwarz <christof.schwarz@gmx.at>
2019-09-12 15:55:40 +0200

    Update and rename restart_pods.ps1 to delete_pods.ps1

---
+++ delete_pods.ps1
@@ -0,0 +1,16 @@
+# This Powershell will call "kubectl delete pod" for a given list of pods
+# (...typically after deletion they will be restarted by the deployment object ...)
+# Provide the list of pod name fragments as command line arguments, separated by space
+# Each entry is matched as "pod name >contains< entry", not the exact name
+
+if ($args.count -eq 0) {
+  echo "No arguments provided. Give a space-separated list of (fragments of) pod names."
+} else {
+  $podlist = $args -join "|"
+  Write-Host "Deleting pods $podlist ..."
+  $getpods = kubectl get pod -o=custom-columns=n:.metadata.name | Select-String "(?:$podlist)" 
+  ForEach ($pod in ($getpods -split ("`r") -split ("`n")))
+  {
+    If ($pod.length -gt 0) { kubectl delete pod $pod }
+  }
+}

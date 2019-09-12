b4526ef2019f09068e3dc8aed712fc64eea17f7c
Christof Schwarz <christof.schwarz@gmx.at>
2019-09-12 15:56:16 +0200

    Update delete_pods.ps1

--- delete_pods.ps1
+++ delete_pods.ps1
@@ -2,6 +2,7 @@
 # (...typically after deletion they will be restarted by the deployment object ...)
 # Provide the list of pod name fragments as command line arguments, separated by space
 # Each entry is matched as "pod name >contains< entry", not the exact name
+# example: ./delete_pods.ps1 auth sess group
 
 if ($args.count -eq 0) {
   echo "No arguments provided. Give a space-separated list of (fragments of) pod names."
